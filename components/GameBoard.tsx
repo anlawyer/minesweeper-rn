46021775982d3dedad69e59e54d416d4991f628c
anlawyer <anlawyer@berkeley.edu>
2021-08-07 20:57:10 -0400

    Basic grid rendering

--- components/GameBoard.tsx
+++ components/GameBoard.tsx
@@ -6,23 +6,39 @@ interface GameBoardProps {
     params: any;
 }
 
+const Row = ({ row }: { row: any[] }) => (
+    <View style={styles.row}>
+        {row.map((col: number, index: number) => {
+            return (
+                <Tile open={false} content={col} key={`col-${index}`} />
+            )
+        })}
+    </View>
+);
+
 export default function GameBoard({ params }: GameBoardProps) {
-    const [board, setBoard] = useState([] as any[]);
+    const [board, setBoard] = useState([] as any[][]);
     useEffect(() => {
+        if (board.length === params.rows) return;
         for(let i = 0; i < params.rows; i++) {
             let row = Array(params.cols);
             row.fill(0);
             board.push(row);
         }
         setBoard(board);
-    }, [params]);
+    }, []);
 
     return (
       <View>
-        {board.map(row => row.map((col: number) => <Tile open={false} content={col} />))}
+        {board.map((row, index) => <Row row={row} key={`row-${index}`} />)}
       </View>
     );
   }
   
-  const styles = StyleSheet.create({});
+  const styles = StyleSheet.create({
+      row: {
+          display: 'flex',
+          flexDirection: 'row',
+      }
+  });
   
