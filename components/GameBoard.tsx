c24e1e667608a767d233fbb06e93daae1a46ac86
anlawyer <anlawyer@berkeley.edu>
2021-08-09 22:00:55 -0400

    WIP neighbor generation

--- components/GameBoard.tsx
+++ components/GameBoard.tsx
@@ -8,20 +8,49 @@ interface GameBoardProps {
 
 const Row = ({ row }: { row: any[] }) => (
   <View style={styles.row}>
-    {row.map((col: number, index: number) => {
-      return <Tile open={false} content={col} key={`col-${index}`} />;
+    {row.map((col: any, index: number) => {
+      return <Tile open content={col.content} key={`col-${index}`} />;
     })}
   </View>
 );
 
+const defaultTile = {
+  content: 0,
+  neighbors: [],
+};
+
+const getTile = (array: any[][], rowIndex: number, colIndex: number) => {
+  // NOTE: inspired by https://kozmicluis.com/adjacent-cells-of-a-matrix/
+  var NO_VALUE = null;
+  var value, hasValue;
+
+  try {
+    hasValue = array[rowIndex][colIndex] !== undefined;
+    value = hasValue ? array[rowIndex][colIndex] : NO_VALUE;
+  } catch (e) {
+    value = NO_VALUE;
+  }
+
+  return value;
+};
+
+const setTileContent = (
+  tile: any,
+  { content, neighbors }: { content?: any; neighbors?: any }
+) => {
+  return { ...tile, content, neighbors };
+};
+
 export default function GameBoard({ params }: GameBoardProps) {
   const [board, setBoard] = useState([] as any[][]);
   const [bombsPlaced, setBombsPlaced] = useState(false);
+
+  // Generate empty game board: fill rows, columns with empty tiles
   useEffect(() => {
     if (board.length === params.rows) return;
     for (let i = 0; i < params.rows; i++) {
       let row = Array(params.cols);
-      row.fill(0);
+      row.fill(defaultTile);
       board.push(row);
     }
     setBoard(board);
@@ -29,23 +58,49 @@ export default function GameBoard({ params }: GameBoardProps) {
 
   const placeBombs = useCallback((numBombs: number) => {
     let bombsRemaining = numBombs;
-
     while (bombsRemaining > 0) {
       const randomColIndex = Math.floor(Math.random() * params.cols);
       const randomRowIndex = Math.floor(Math.random() * params.rows);
-      let selectedTile = board[randomRowIndex][randomColIndex];
-      if (selectedTile !== "bomb") {
+      let selectedTile = getTile(board, randomRowIndex, randomColIndex);
+      if (selectedTile.content !== "bomb") {
         const selectedRow = board[randomRowIndex];
-        selectedRow.splice(randomColIndex, 1, "bomb");
+        const updatedTile = setTileContent(selectedTile, { content: "bomb" });
+        selectedRow.splice(randomColIndex, 1, updatedTile);
         board.splice(randomRowIndex, 1, selectedRow);
         bombsRemaining -= 1;
       }
     }
   }, []);
 
+  const generateTilesNeighbors = (r: number, c: number) => {
+    const neighbors = {
+      up: getTile(board, r - 1, c),
+      upRight: getTile(board, r - 1, c + 1),
+      right: getTile(board, r, c + 1),
+      downRight: getTile(board, r + 1, c + 1),
+      down: getTile(board, r + 1, c),
+      downLeft: getTile(board, r + 1, c - 1),
+      left: getTile(board, r, c - 1),
+      upLeft: getTile(board, r - 1, c - 1),
+    };
+    const tile = getTile(board, r, c);
+    const selectedRow = board[r];
+    const updatedTile = setTileContent(tile, { neighbors });
+  };
+
+  const setAllNeighbors = () => {
+    board.forEach((row, rIndex) => {
+      row.forEach((col, cIndex) => {
+        generateTilesNeighbors(rIndex, cIndex);
+      });
+    });
+  };
+
+  // Call function to put bombs in random tiles, if game board is ready
   useEffect(() => {
     if (board.length === params.rows && !bombsPlaced) {
       placeBombs(params.bombs);
+      setAllNeighbors();
       setBombsPlaced(true);
     }
   }, [board.length]);
